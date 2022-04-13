471174ee6682b6dfe7ac0d8ce97a4d133da35627
Alejandro Visiedo <avisiedo@redhat.com>
2022-04-13 08:04:27 +0200

    Initial commit

---
+++ fec.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	appUrl: ['/insights/insights-idm'],
+	debug: true,
+	useProxy: true,
+	proxyVerbose: true,
+	/**
+	 * Change to false after your app is registered in configuration files
+	 */
+	interceptChromeConfig: true,
+	/**
+	 * Add additional webpack plugins
+	 */
+	plugins: [],
+};
